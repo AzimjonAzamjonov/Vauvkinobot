59bbfe1327a323812eea540e152e668b44f866a2
Azimjon Azamjonov <107868762+AzimjonAzamjonov@users.noreply.github.com>
2025-08-03 00:39:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 # ─── ENV / CONST ─────────────────────────────────────────────────────
 BOT_TOKEN        = os.getenv("BOT_TOKEN",        "YOUR_TOKEN_HERE")
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "@filmxona_kodlari")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "@kinokodlarida")
 ADMIN_CODE       = os.getenv("ADMIN_CODE",       "2299")
 
 DATA_FILE   = "/data1/kino_data.json"
